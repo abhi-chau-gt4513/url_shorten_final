fc015bf9c39d61b37fd19a71b11c444e528596ef
achauhan63 <achauhan63@gatech.edu>
2022-01-18 02:10:52 -0500

    Redirecting and the entire prototype works.

--- ui/src/components/URLShortenerForm.tsx
+++ ui/src/components/URLShortenerForm.tsx
@@ -1,5 +1,51 @@
+import { Input, Button, Box, Heading, InputGroup } from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+import {SERVER_ENDPOINTS} from "../config";
+
 function URLShortenerForm() {
-    return <div>hi</div>
+    const [destination, setDestination] = useState();
+
+    const [shortUrl, setShortUrl] = useState<{
+        shortId: string
+    } | null>(null);
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        setShortUrl(null);
+
+        const result = await axios.post(`${SERVER_ENDPOINTS}/api/url`,{
+            destination,
+        })
+        .then((resp) => resp.data);
+
+        setShortUrl(result);
+
+        debugger;
+
+        // console.log(result);
+    }
+
+    return <Box pos="relative">
+
+        <form onSubmit={handleSubmit}>
+            {/* destination: {destination} */}
+            <Input 
+                onChange={(e: any) => setDestination(e.target.value)}
+                placeholder="https://example.com"
+            />,
+            <Button type="submit">
+                CREATE SHORT URL
+            </Button>
+        </form>,
+        {/* {JSON.stringify(shortUrl)} */}
+        {shortUrl && (
+            <a href={`${SERVER_ENDPOINTS}/${shortUrl?.shortId}`}>
+                CLICK ME
+            </a>
+        )}
+    </Box>
 }
 
 export default URLShortenerForm;
