a98b60f50cecdc4eb71a94ea1754253d52a8cb73
achauhan63 <achauhan63@gatech.edu>
2022-01-20 02:41:00 -0500

    Prototype app finished and backed by MongoDB.

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,5 +1,6 @@
 export default {
     port: 4000,
-    dbUri: "mongodb://localhost:27017/url-shortener",
+    dbUri: "mongodb+srv://abhi:root123456@cluster0.d3fvp.mongodb.net/url-shortener?retryWrites=true&w=majority",
+    // dbUri: "mongodb://localhost:27017/url-shortener",
     corsOrigin: "http://localhost:3000"
 }
